3711681691523299127acb4a997c81b2c3fd46a4
danielperaltamadriz <b04740@ECCI-LBW-106-15>
2016-04-08 10:35:18 -0600

    nuevo

---
+++ Activo_prestamos_OET/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Activo_prestamos_OET
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Activo_prestamos_OET/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Activo_prestamos_OET.MvcApplication" Language="C#" %>
